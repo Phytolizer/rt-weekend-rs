0457ab5828954746cb29a15af14352e9effed8ba
Kyle Coffey <me@phytolizer.dev>
2023-03-19 23:36:30 -0500

    final render

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use clap::Parser;
 use color::write_color;
 use hittable::Hittable;
 use image::RgbImage;
+use indicatif::ParallelProgressIterator;
 use indicatif::ProgressBar;
 use itertools::Itertools;
 use material::ScatterRecord;
@@ -15,6 +16,7 @@ use ray::Ray;
 use rayon::prelude::{IntoParallelIterator, ParallelBridge, ParallelIterator};
 
 use crate::material::dielectric::Dielectric;
+use crate::material::Material;
 use crate::{
     camera::Camera,
     hittable::{hittable_list::HittableList, sphere::Sphere},
@@ -109,55 +111,83 @@ enum State {
     Offline,
 }
 
+fn random_scene() -> Box<dyn Hittable> {
+    let mut world = HittableList::new();
+
+    let ground_material = Arc::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
+    world.add(Box::new(Sphere::new(
+        Point3::new(0.0, -1000.0, 0.0),
+        1000.0,
+        ground_material,
+    )));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = rand::thread_rng().gen_range(0.0..1.0);
+            let center = Point3::new(
+                a as f64 + 0.9 * rand::thread_rng().gen_range(0.0..1.0),
+                0.2,
+                b as f64 + 0.9 * rand::thread_rng().gen_range(0.0..1.0),
+            );
+
+            if (center - Vec3::new(4.0, 0.2, 0.0)).magnitude() > 0.9 {
+                let sphere_material: Arc<dyn Material> = if choose_mat < 0.8 {
+                    // diffuse
+                    let albedo: Vec3 = random_vec().component_mul(&random_vec()).into();
+                    Arc::new(Lambertian::new(albedo))
+                } else if choose_mat < 0.95 {
+                    // metal
+                    let albedo = random_vec_range(0.5, 1.0);
+                    let fuzz = rand::thread_rng().gen_range(0.0..0.5);
+                    Arc::new(Metal::new(albedo, fuzz))
+                } else {
+                    // glass
+                    Arc::new(Dielectric::new(1.5))
+                };
+
+                world.add(Box::new(Sphere::new(center, 0.2, sphere_material)));
+            }
+        }
+    }
+
+    let material1 = Arc::new(Dielectric::new(1.5));
+    world.add(Box::new(Sphere::new(
+        Point3::new(0.0, 1.0, 0.0),
+        1.0,
+        material1,
+    )));
+
+    let material2 = Arc::new(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
+    world.add(Box::new(Sphere::new(
+        Point3::new(-4.0, 1.0, 0.0),
+        1.0,
+        material2,
+    )));
+
+    let material3 = Arc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
+    world.add(Box::new(Sphere::new(
+        Point3::new(4.0, 1.0, 0.0),
+        1.0,
+        material3,
+    )));
+
+    Box::new(world)
+}
+
 fn main() {
-    const aspect_ratio: f64 = 16.0 / 9.0;
-    const image_width: usize = 400;
+    const aspect_ratio: f64 = 3.0 / 2.0;
+    const image_width: usize = 1200;
     const image_height: usize = (image_width as f64 / aspect_ratio) as usize;
-    const samples_per_pixel: usize = 400;
+    const samples_per_pixel: usize = 500;
     const max_depth: usize = 50;
 
-    let r = (f64::consts::PI / 4.0).cos();
+    let world = random_scene();
 
-    let material_ground = Arc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
-    let material_center = Arc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
-    let material_left = Arc::new(Dielectric::new(1.5));
-    let material_right = Arc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
-
-    let world = {
-        let mut world = HittableList::new();
-        world.add(Box::new(Sphere::new(
-            Point3::new(0.0, -100.5, -1.0),
-            100.0,
-            material_ground,
-        )));
-        world.add(Box::new(Sphere::new(
-            Point3::new(0.0, 0.0, -1.0),
-            0.5,
-            material_center,
-        )));
-        world.add(Box::new(Sphere::new(
-            Point3::new(-1.0, 0.0, -1.0),
-            0.5,
-            material_left.clone(),
-        )));
-        world.add(Box::new(Sphere::new(
-            Point3::new(-1.0, 0.0, -1.0),
-            -0.4,
-            material_left,
-        )));
-        world.add(Box::new(Sphere::new(
-            Point3::new(1.0, 0.0, -1.0),
-            0.5,
-            material_right,
-        )));
-        world
-    };
-
-    let lookfrom = Point3::new(3.0, 3.0, 2.0);
-    let lookat = Point3::new(0.0, 0.0, -1.0);
+    let lookfrom = Point3::new(13.0, 2.0, 3.0);
+    let lookat = Point3::new(0.0, 0.0, 0.0);
     let vup = Vec3::new(0.0, 1.0, 0.0);
-    let dist_to_focus = (lookfrom - lookat).magnitude();
-    let aperture = 2.0;
+    let dist_to_focus = 10.0;
+    let aperture = 0.1;
 
     let camera = Camera::new(
         lookfrom,
@@ -199,15 +229,17 @@ fn main() {
     let progress = ProgressBar::new((image_height * image_width) as u64)
         .with_style(
             indicatif::ProgressStyle::default_bar()
-                .template("[{elapsed_precise}] {bar:40} {pos:>7}/{len:7} {msg}")
+                .template("[{elapsed_precise}/{duration_precise}] {bar:40} {pos:>7}/{len:7} {msg}")
                 .unwrap(),
         )
         .with_message(format!("Rendering {image_width}x{image_height}"));
-    for (x, y, color) in (0..image_height)
+    let (tx, rx) = crossbeam_channel::unbounded::<(usize, usize, Color)>();
+    (0..image_height)
         .cartesian_product(0..image_width)
         .par_bridge()
         .into_par_iter()
-        .map(|(j, i)| {
+        .progress_with(progress)
+        .for_each_with(tx, |tx, (j, i)| {
             let mut pixel_color = Color::new(0.0, 0.0, 0.0);
             for _ in 0..samples_per_pixel {
                 let u =
@@ -215,16 +247,13 @@ fn main() {
                 let v =
                     (j as f64 + rand::thread_rng().gen_range(0.0..1.0)) / (image_height - 1) as f64;
                 let r = camera.get_ray(u, v);
-                pixel_color += ray_color(&r, &world, max_depth);
+                pixel_color += ray_color(&r, world.as_ref(), max_depth);
             }
-            progress.inc(1);
-            (i, image_height - j - 1, pixel_color)
-        })
-        .collect::<Vec<_>>()
-    {
+            tx.send((i, image_height - j - 1, pixel_color)).unwrap();
+        });
+    while let Ok((x, y, color)) = rx.recv() {
         write_color(&mut img, (x, y), &color, samples_per_pixel);
     }
-    progress.finish();
 
     match state {
         State::Online(mut state) => {
