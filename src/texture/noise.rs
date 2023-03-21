8f8a3eb07adc4e752adca77cadde7f3dcfd19409
Kyle Coffey <me@phytolizer.dev>
2023-03-21 14:57:58 -0500

    marble texture

--- src/texture/noise.rs
+++ src/texture/noise.rs
@@ -20,6 +20,9 @@ impl NoiseTexture {
 
 impl Texture for NoiseTexture {
     fn value(&self, _u: f64, _v: f64, p: Point3) -> Color {
-        Color::new(1.0, 1.0, 1.0) * self.noise.turb(self.scale * p, Perlin::DEFAULT_TURB_DEPTH)
+        Color::new(1.0, 1.0, 1.0)
+            * 0.5
+            * (1.0
+                + (self.scale * p.z + 10.0 * self.noise.turb(p, Perlin::DEFAULT_TURB_DEPTH)).sin())
     }
 }
